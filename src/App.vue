c582d824be3f90f7dcbba0af308833df2f482365
凉风有信 <3042146237@qq.com>
2021-09-20 16:02:21 +0800

    feat:初始化

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" age="12" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld,
   },
-});
+})
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { createApp, defineComponent, h } from 'vue'
+// import App from './App.vue'
+const IMG = require('./assets/logo.png')
 
-createApp(App).mount("#app");
+const App = defineComponent({
+  render() {
+    return h('div', { id: 'app' }, [
+      h('img', {
+        alt: 'vue img',
+        src: IMG,
+      }),
+      h('h1', { id: 'h1' }, 'hello vue3'),
+    ])
+  },
+})
+
+createApp(App).mount('#app')
