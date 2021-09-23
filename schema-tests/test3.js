6e9460af8bbe77871f1402e78f3670b4643b4fd4
凉风有信 <3042146237@qq.com>
2021-09-23 19:59:54 +0800

    feat:第四章

---
+++ schema-tests/test3.js
@@ -0,0 +1,88 @@
+/**
+ * @description 自定义关键字
+ * @author 凉风有信、
+ */
+
+const Ajv = require("ajv")
+const localize = require("ajv-i18n")
+const ajv = new Ajv({
+  allErrors: true,
+  jsonPointers: true
+}) // options can be passed, e.g. {allErrors: true}
+require('ajv-errors')(ajv)
+
+// ajv.addFormat('test', (data) => {
+//   console.log(data, '--------------')
+//   return data === 'data'
+// })
+
+ajv.addKeyword('test', {
+  // validate: function fun(schema, data) {
+
+  //   fun.errors = [{
+  //     keyword: 'test',
+  //     dataPath: '.name',
+  //     schemaPath: '#/properties/name/test',
+  //     params: {
+  //       keyword: 'test'
+  //     },
+  //     message: '这是自定义的错误信息！'
+  //   }]
+  //   // console.log(schema, data, '----------');
+  //   return false
+  // },
+  // compile(schema, parentSchema) {
+  //   console.log(schema, parentSchema);
+  //   return () => true
+  // },
+  // metaSchema: {
+  //   type: 'boolean'
+  // },
+  macro: function (schema, parentSchema) {
+    return {
+      minLength: 10
+    }
+  }
+})
+
+const schema = {
+  type: "object",
+  properties: {
+    name: {
+      type: 'string',
+      // test: true,
+      errorMessage: {
+        minLength: '长度不对'
+      },
+      minLength: 10
+    },
+    age: {
+      type: 'number'
+    },
+    pets: {
+      type: 'array',
+      items: {
+        type: 'string'
+      }
+    },
+    isworker: {
+      type: 'boolean'
+    }
+  },
+  required: ['name', 'age']
+}
+
+const data = {
+  name: 'liang',
+  age: 19,
+  pets: ['mimi', 'wawa'],
+  isworker: true
+}
+
+const validate = ajv.compile(schema)
+
+const valid = validate(data)
+if (!valid) {
+  localize.zh(validate.errors)
+  console.log(validate.errors)
+}
